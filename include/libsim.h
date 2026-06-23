700a1491e395b32ec20d90b0a4cb45b4fc74c94f
BPoroshin <mrbogdanello@gmail.com>
2026-06-23 13:22:11 +0300

    Initial commit

---
+++ include/libsim.h
@@ -0,0 +1,12 @@
+#ifndef LIBSIM_H
+#define LIBSIM_H
+
+#include "libsim_global.h"
+
+class LIBSIM_EXPORT Libsim
+{
+public:
+    Libsim();
+};
+
+#endif // LIBSIM_H
---
+++ src/core/libsim.cpp
@@ -0,0 +1,3 @@
+#include "libsim.h"
+
+Libsim::Libsim() {}
